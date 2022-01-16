dbe629dac97622bd22f224fa7c1c1786bf534c80
Jon Allured <jon.allured@gmail.com>
2022-01-16 13:25:59 -0600

    Shuffle some tests around

--- typescript/src/FortyTime.ts
+++ typescript/src/FortyTime.ts
@@ -1,64 +1,7 @@
-import { BaseFortyTime } from "./BaseFortyTime"
-import { NullFortyTime } from "./NullFortyTime"
+import { Amount } from "./Amount"
+import { Point } from "./Point"
 
-export class FortyTime extends BaseFortyTime {
-  static parse = (input: number | string | null): BaseFortyTime => {
-    if (input === null || input === "") {
-      return new NullFortyTime()
-    }
-
-    if (typeof input === "number") {
-      if (Number.isInteger(input)) {
-        return new FortyTime(input)
-      } else {
-        throw new FortyTime.ParseError()
-      }
-    }
-
-    if (!input.match(/:/)) throw new FortyTime.ParseError()
-
-    // eslint-disable-next-line prefer-const
-    let [hours, extra] = input.split(":").map(Number)
-
-    if (input[0] === "-") {
-      extra = extra * -1
-    }
-
-    const minutes = hours * 60 + extra
-
-    return new FortyTime(minutes)
-  }
-
-  get value(): number {
-    return this.minutes
-  }
-
-  toString = (): string => {
-    let hours: string | number = Math.trunc(this.minutes / 60)
-    let extra: string | number = this.minutes - hours * 60
-
-    if (this.minutes < 0) {
-      hours = hours * -1
-      extra = extra * -1
-      hours = `-${hours}`
-    }
-
-    if (extra < 10) {
-      extra = `0${extra}`
-    }
-
-    return [hours, extra].join(":")
-  }
-
-  plus = (other: BaseFortyTime): BaseFortyTime => {
-    const sum = this.minutes + other.minutes
-    const result = new FortyTime(sum)
-    return result
-  }
-
-  minus = (other: BaseFortyTime): BaseFortyTime => {
-    const diff = this.minutes - other.minutes
-    const result = new FortyTime(diff)
-    return result
-  }
+export const FortyTime = {
+  Amount,
+  Point,
 }
